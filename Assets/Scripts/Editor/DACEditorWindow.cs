21d8e9e315b39770f6d68d02f5a146f787fa3b08
Nguyễn Minh Đức <boydbvn@gmail.com>
2025-08-10 17:55:57 +0700

    update node

--- Assets/Scripts/Editor/DACEditorWindow.cs
+++ Assets/Scripts/Editor/DACEditorWindow.cs
@@ -1,15 +1,41 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-namespace EW.windows
+namespace DATN2.Editor
 {
     public class DACEditorWindow : EditorWindow
     {
         [MenuItem("DAC/Cutscene Controller Graph")]
-        public static void ShowExample()
+        public static void Open()
         {
-            DACEditorWindow editorWindow = GetWindow<DACEditorWindow>();
-            editorWindow.titleContent = new GUIContent("Cutscene Controller Graph");
+            GetWindow<DACEditorWindow>("DAC Editor Window");
+        }
+        void OnEnable()//co the switch qua createGUi
+        {
+            AddGraphView();
+
+            AddStyle();
+        }
+        void AddGraphView()
+        {
+            DACGraphView graphview = new DACGraphView();
+            graphview.StretchToParentSize();//phong to graphview = editor
+            rootVisualElement.Add(graphview);
+        }
+        void AddStyle()
+        {
+            StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/Editor/DialogSystem/Windows/DACVariables.uss");
+
+            if (styleSheet != null)
+            {
+                rootVisualElement.styleSheets.Add(styleSheet);
+            }
+            else
+            {
+                Debug.LogError("Failed to load DACVariables.uss - Check path: Assets/Scripts/Editor/DialogSystem/Windows/DACVariables.uss");
+            }
+            rootVisualElement.styleSheets.Add(styleSheet);
         }
     }
 
