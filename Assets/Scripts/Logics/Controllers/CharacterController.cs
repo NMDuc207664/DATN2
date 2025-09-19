d6768ed39690d57f98b5212f1a25f0d6c62e71a3
Nguyễn Minh Đức <boydbvn@gmail.com>
2025-09-19 19:30:40 +0700

    update extension

--- Assets/Scripts/Logics/Controllers/CharacterController.cs
+++ Assets/Scripts/Logics/Controllers/CharacterController.cs
@@ -29,10 +29,7 @@ namespace DATN2.Assets.Scripts.Logics.Controllers
         [SerializeField] private PhysicMaterial slipperyMaterial;
         [SerializeField] private BoxCollider _groundCheckCollider; // Reference to BoxCollider
 
-        [SerializeField] GameObject stepRayUpper;
-        [SerializeField] GameObject stepRayLower;
-        [SerializeField] float stepHeight = 0.3f;
-        [SerializeField] float stepSmooth = 2f;
+
 
         bool isPickingUp = false;
         private Collider _collider;
@@ -50,7 +47,6 @@ namespace DATN2.Assets.Scripts.Logics.Controllers
         private void FixedUpdate()
         {
             HandleMovementInput();
-            StepClimb();
 
         }
         private void Update()
@@ -212,39 +208,6 @@ namespace DATN2.Assets.Scripts.Logics.Controllers
         //     }
         // }
         // --- Hàm StepClimb mới ---
-        void StepClimb()
-        {
-            RaycastHit hitLower;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(Vector3.forward), out hitLower, 0.1f))
-            {
-                RaycastHit hitUpper;
-                if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(Vector3.forward), out hitUpper, 0.2f))
-                {
-                    _rigidbody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
-                }
-            }
 
-            RaycastHit hitLower45;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitLower45, 0.1f))
-            {
-
-                RaycastHit hitUpper45;
-                if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(1.5f, 0, 1), out hitUpper45, 0.2f))
-                {
-                    _rigidbody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
-                }
-            }
-
-            RaycastHit hitLowerMinus45;
-            if (Physics.Raycast(stepRayLower.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitLowerMinus45, 0.1f))
-            {
-
-                RaycastHit hitUpperMinus45;
-                if (!Physics.Raycast(stepRayUpper.transform.position, transform.TransformDirection(-1.5f, 0, 1), out hitUpperMinus45, 0.2f))
-                {
-                    _rigidbody.position -= new Vector3(0f, -stepSmooth * Time.deltaTime, 0f);
-                }
-            }
-        }
     }
 }
